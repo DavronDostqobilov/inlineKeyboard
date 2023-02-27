134262c15abb4d8d8611dc63f70fe3d4d03099c6
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-02-27 12:11:24 +0500

    inline_markup

--- main.py
+++ main.py
@@ -8,30 +8,66 @@ from telegram.ext import (
     CommandHandler, 
     CallbackQueryHandler)
 import os
+import json
 
 TOKEN=os.environ['TOKEN']
 
+updater = Updater(TOKEN)
+dp = updater.dispatcher
+
 def start(update: Update, context: CallbackContext):
     bot = context.bot
-    
-    chat_id = update.message.chat.id
-    text = update.message.text
+    chat_id = str(update.message.chat.id)
+    try:
+        with open('data.json','r') as f:
+            data=json.loads(f.read())
+        like = data[chat_id]['like']
+        dislike = data[chat_id]['dislike']
+    except:
+        data={chat_id:{'like':0,'dislike':0}}
+        with open('data.json','w') as f:
+            json.dump(data,fp=f, indent=4)
     bot.sendMessage(chat_id=chat_id, text="Send me a Photo")
 
 def photo(update: Update, context: CallbackContext):
     bot = context.bot
-    chat_id = update.message.chat.id
+    chat_id = str(update.message.chat.id)
     photo = update.message.photo[-1]["file_id"]
+    with open('data.json','r') as f:
+        data=json.loads(f.read())
+    like = data[chat_id]['like']
+    dislike = data[chat_id]['dislike']
 
-    button1 = InlineKeyboardButton(text = "👍", callback_data="like")
-    button2 = InlineKeyboardButton(text = "👎", callback_data="dislike")
+    button1 = InlineKeyboardButton(text = f"👍 {like}", callback_data="like")
+    button2 = InlineKeyboardButton(text = f"👎 {dislike}", callback_data="dislike")
 
     keyboard = InlineKeyboardMarkup([[button1, button2]])
     bot.sendPhoto(chat_id=chat_id, photo=photo, reply_markup=keyboard)
 
 def like_and_dislike(update: Update, context: CallbackContext):
     query = update.callback_query
+    chat_id=update.message.chat.id
+    bot = context.bot
     print(query.data)
+    with open('data.json','r') as f:
+        data=json.loads(f.read())    
+    like = data[f'{chat_id}']['like']
+    dislike = data[f'{chat_id}']['dislike']
+    if query.data=='like':
+        like+=1
+    if query.data=='dislike':
+        dislike+=1
+    print(like)
+    print(dislike)
+    data[f'{chat_id}']['like'] = like
+    data[f'{chat_id}']['dislike'] = dislike
+    with open('data.json','w') as f:
+        json.dump(data,fp=f, indent=4)
+    button1 = InlineKeyboardButton(text = f"👍 {like}", callback_data="like")
+    button2 = InlineKeyboardButton(text = f"👎 {dislike}", callback_data="dislike")
+
+    keyboard = InlineKeyboardMarkup([[button1, button2]])
+    update.callback_query.message.edit_reply_markup(chat_id=chat_id, reply_markup=keyboard)
 
 updater = Updater(TOKEN)
 dp = updater.dispatcher
